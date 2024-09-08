02cd3f1c321e86befdfe9200c6fcd8a50d938391
Anurag <anurag2002sharma@gmail.com>
2024-09-08 06:56:48 +0530

    ui for navigation

---
+++ easy-stay/app/components/Avatar.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react'
+import Image from 'next/image';
+import { FaUserCircle } from "react-icons/fa";
+
+const Avatar= () => {
+  return (
+    <FaUserCircle size={30} color='gray'/>
+  )
+}
+
+export default Avatar
