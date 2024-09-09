59273f1be822b0a4724e8c32441599648c27fba8
Anurag <anurag2002sharma@gmail.com>
2024-09-10 02:57:04 +0530

    added modals

--- easy-stay/app/layout.tsx
+++ easy-stay/app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Nunito } from "next/font/google";
 import { Navbar } from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,6 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <RegisterModal/>
         <Navbar/>
         {children}
       </body>
