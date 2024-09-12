5b9d70973e8512de38292a15553c801a5551cdbf
Anurag <anurag2002sharma@gmail.com>
2024-09-13 01:47:05 +0530

    fixing issues

--- easy-stay/app/components/navbar/UserMenu.tsx
+++ easy-stay/app/components/navbar/UserMenu.tsx
@@ -1,10 +1,17 @@
-'use client';
-import React, { useCallback } from 'react'
-import { AiOutlineMenu } from 'react-icons/ai';
-import Avatar from '../Avatar';
-import { useState } from 'react';
-import MenuItem from './MenuItem';
-import useRegisterModal from '@/app/hooks/useRegisterModal';
+"use client";
+import React, { useCallback } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+import { useState } from "react";
+import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import {signOut} from "next-auth/react"
+
+interface UserMenuProps {
+  currentUser?: User | null;
+}
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
   const registerModal = useRegisterModal();
@@ -53,16 +60,17 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                     cursor-pointer
                     hover:shadow-md
                     transition
-                '
-            >
-                <AiOutlineMenu/>
-                <div className='hidden md:block'>
-                    <Avatar/>
-                </div>
-            </div>
+                "
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar src={currentUser?.image} />
+          </div>
         </div>
-        {isOpen && (
-            <div className='
+      </div>
+      {isOpen && (
+        <div
+          className="
                 absolute
                 rounded-xl
                 shadow-md
