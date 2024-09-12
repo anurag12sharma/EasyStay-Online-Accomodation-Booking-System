9e60a500007beecdbc0066ee6e3acaf251707f5d
Anurag <anurag2002sharma@gmail.com>
2024-09-13 00:22:22 +0530

    added google and github login

--- easy-stay/app/components/Avatar.tsx
+++ easy-stay/app/components/Avatar.tsx
@@ -4,10 +4,19 @@ import React from 'react'
 import Image from 'next/image';
 import { FaUserCircle } from "react-icons/fa";
 
-const Avatar= () => {
+interface AvatarProps{
+  src: string | null | undefined
+};
+
+const Avatar:React.FC<AvatarProps>= ({
+  src
+}) => {
   return (
-    <FaUserCircle size={30} color='gray'/>
-  )
+    src &&
+    <Image
+      className='rounded-full' height={30}  width={30}  alt='avatar' src={src}
+    /> || <FaUserCircle size={30} color='gray'/>
+    )
 }
 
 export default Avatar
