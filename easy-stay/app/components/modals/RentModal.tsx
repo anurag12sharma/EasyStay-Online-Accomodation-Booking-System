30e174532d0216e3f4f8e69097815c56bc1c3909
Anurag <anurag2002sharma@gmail.com>
2024-09-13 15:34:27 +0530

    added map component

--- easy-stay/app/components/modals/RentModal.tsx
+++ easy-stay/app/components/modals/RentModal.tsx
@@ -7,6 +7,8 @@ import Heading from '../Heading';
 import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
+import CountrySelect from '../inputs/CountrySelect';
+import dynamic from 'next/dynamic';
 
 enum STEPS{
     CATEGORY = 0,
@@ -16,6 +18,8 @@ enum STEPS{
     DESCRIPTION = 4,
     PRICE = 5
 }
+
+
 
 const RentModal = () => {
     const rentModal = useRentModal();
@@ -45,6 +49,11 @@ const RentModal = () => {
     });
 
     const category = watch('category');
+    const location = watch('location');
+
+    const Map = useMemo(()=>dynamic(()=> import('../Map'),{
+        ssr:false
+    }),[location]);
 
     const setCustomValue = (id:string, value:any) => {
         setValue(id,value,{
@@ -106,11 +115,30 @@ const RentModal = () => {
         </div>
     )
 
+    if(step===STEPS.LOCATION){
+        bodyContent = (
+            <div className='flex flex-col gap-8'>
+                <Heading
+                    title='Where is your place located?'
+                    subtitle='Help guests find you!'
+                />
+                <CountrySelect
+                    value={location}
+                    onChange={(value)=>setCustomValue('location',value)}
+                />
+                <Map
+                    center={location?.latlng}
+                />
+            </div>
+            
+        )
+    }
+
   return (
     <Modal
         isOpen = {rentModal.isOpen}
         onClose={rentModal.onClose}
-        onSubmit={rentModal.onClose}
+        onSubmit={onNext}
         actionLable={actionLabel}
         secondaryActionLabel={secondaryActionLabel}
         secondaryAction={step===STEPS.CATEGORY?undefined:onBack}
