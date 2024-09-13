be3b1479f9b646bd2b346b97c6aab6cadf30275c
Anurag <anurag2002sharma@gmail.com>
2024-09-13 17:20:13 +0530

    added property registration form with guest, room, and bathroom input functionality

--- easy-stay/app/components/modals/RentModal.tsx
+++ easy-stay/app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import CategoryInput from '../inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
 import dynamic from 'next/dynamic';
+import Counter from '../inputs/Counter';
 
 enum STEPS{
     CATEGORY = 0,
@@ -50,6 +51,9 @@ const RentModal = () => {
 
     const category = watch('category');
     const location = watch('location');
+    const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
 
     const Map = useMemo(()=>dynamic(()=> import('../Map'),{
         ssr:false
@@ -131,6 +135,37 @@ const RentModal = () => {
                 />
             </div>
             
+        )
+    }
+
+    if(step===STEPS.INFO){
+        bodyContent = (
+            <div className='flex flex-col gap-8'>
+                <Heading
+                    title='Tell Us About Your Space'
+                    subtitle='What amenities do you provide?'
+                />
+                <Counter 
+                    title='Guests' 
+                    subtitle='How many guests will stay?'
+                    value= {guestCount}
+                    onChange={(value)=>setCustomValue('guestCount',value)}
+                />
+                <hr />
+                <Counter 
+                    title='Rooms' 
+                    subtitle='How many rooms are available?'
+                    value= {roomCount}
+                    onChange={(value)=>setCustomValue('roomCount',value)}
+                />
+                <hr />
+                <Counter 
+                    title='Bathrooms' 
+                    subtitle='How many bathrooms does your place have?'
+                    value= {bathroomCount}
+                    onChange={(value)=>setCustomValue('bathroomCount',value)}
+                />
+            </div>
         )
     }
 
